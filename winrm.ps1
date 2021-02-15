68bbab23d7e4d1284d2dfe2d7ed48d475e98380a
Mairien Anthony <54004140+IT-Anthony@users.noreply.github.com>
2021-02-15 16:29:48 +0100

    Ajout script winrm
    Simple ajout du script winrm.ps1

---
+++ winrm.ps1
@@ -0,0 +1,20 @@
+# Script permettant d'activer le service WinRM.
+# Mairien Anthony - Notamax
+# Mis à jour le 15-02-21
+# Librement repris d'autres templates
+
+$ErrorActionPreference = "Stop"
+
+# Switch network connection to private mode
+# Required for WinRM firewall rules
+$profile = Get-NetConnectionProfile
+Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
+
+# Enable WinRM service
+winrm quickconfig -quiet
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+
+# Reset auto logon count
+# https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
